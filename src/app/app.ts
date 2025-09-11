e3c706a92dce3adfb206dddab508a498235c35d8
Lucas Setiady <luca.setia@gmail.com>
2025-09-11 17:53:55 +1000

    Styling: Adjusted Top Bar Styling and implemented FA modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TopBar } from './top-bar/top-bar';
 import { SidePanel } from './side-panel/side-panel';
 import { CalendarView } from './calendar-view/calendar-view';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBar, SidePanel, CalendarView],
+  imports: [RouterOutlet, TopBar, SidePanel, CalendarView, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/top-bar/top-bar.ts
+++ src/app/top-bar/top-bar.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core'; 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  imports: [DatePipe, FontAwesomeModule],
   templateUrl: './top-bar.html',
   styleUrl: './top-bar.css'
 })
 export class TopBar {
-
+  public currentDate: Date = new Date();
+  public faUser = faUser;
 }
