bbb7422d2acdb9b2b6e3080048cd6c5797533e0d
Lucas Setiady <luca.setia@gmail.com>
2025-09-11 17:42:57 +1000

    Layout/Components: Set up base components and general layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBar } from './top-bar/top-bar';
+import { SidePanel } from './side-panel/side-panel';
+import { CalendarView } from './calendar-view/calendar-view';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBar, SidePanel, CalendarView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('lifePlanner');
+
 }
---
+++ src/app/calendar-view/calendar-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-view',
+  imports: [],
+  templateUrl: './calendar-view.html',
+  styleUrl: './calendar-view.css'
+})
+export class CalendarView {
+
+}
