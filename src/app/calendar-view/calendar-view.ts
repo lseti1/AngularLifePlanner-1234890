cb1f4e3bb0daafdf6f8fd1bb19bc1ffa4d47995f
Lucas Setiady <luca.setia@gmail.com>
2025-09-12 08:41:12 +1000

    Styling: Set up layout for calendar view

--- src/app/calendar-view/calendar-view.ts
+++ src/app/calendar-view/calendar-view.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './calendar-view.css'
 })
 export class CalendarView {
+  public months: string[] = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+  public daysOfWeek: string[] = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  public days: number[] = Array.from({ length: 42 }, (_, i) => i + 1);
 
+  public selectedMonth: number = 0;
+  public selectedDay: number = 0;
 }
