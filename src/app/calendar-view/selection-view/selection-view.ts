0c89f71474eef5c7334ded03f5324d990eb8319c
Lucas Setiady <luca.setia@gmail.com>
2025-09-12 17:09:14 +1000

    Functionality: Made the adding plans more reactive, bringing up a small window if use wants to add plans

--- src/app/calendar-view/selection-view/selection-view.ts
+++ src/app/calendar-view/selection-view/selection-view.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CalendarService } from '../../services/calendar-service';
+
+export type selectionViewType = 'viewing' | 'adding' | 'editing';
 
 @Component({
   selector: 'app-selection-view',
@@ -12,6 +14,9 @@ import { CalendarService } from '../../services/calendar-service';
 export class SelectionView {
   public selectedDatePlans: string[] = ['Do Chores before Church on Sunday', 'Complete Homework', 'Practice Music', 'Complete Homework', 'Practice Music', 'Complete Homework', 'Practice Music'];
   public plan: string = '';
+
+  public selectionViewType = signal<selectionViewType>('viewing');
+  public AddPlansText = computed(() => this.selectionViewType() !== 'adding' ? 'Add Plans' : 'Finish Adding');
 
   constructor(
     private calendarService: CalendarService
@@ -26,5 +31,15 @@ export class SelectionView {
     formData.reset();
     this.calendarService.setHasSelectedDate(false);
     console.log("onSubmit pressed");
+  }
+
+  onAddPlan(): void {
+    this.plan = '';
+  }
+
+  toggleSelectionView(viewType: selectionViewType): void {
+    this.selectionViewType.set(
+      this.selectionViewType() === viewType ? 'viewing' : viewType
+    );
   }
 }
