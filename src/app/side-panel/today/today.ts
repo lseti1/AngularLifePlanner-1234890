3d00d298454db7a79ac4ebd2211a2ba346f73feb
Lucas Setiady <luca.setia@gmail.com>
2025-09-13 16:28:50 +1000

    Functioanlity: Happening Today & Happening Tomorrow now works as intended (also updates automatically, buttons yet to be correctly implemented)

--- src/app/side-panel/today/today.ts
+++ src/app/side-panel/today/today.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { LocalStorageService, Plan } from '../../services/local-storage-service';
+import { CalendarService } from '../../services/calendar-service';
 
 @Component({
   selector: 'app-today',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './today.css'
 })
 export class Today {
+  public todaysPlan = computed(() => this.updateTodaysPlan());
 
+  constructor(
+    private localStorageService: LocalStorageService,
+    private calendarService: CalendarService
+  ) {}
+
+  updateTodaysPlan(): Plan[] {
+    const day = this.calendarService.getCurrentDateDay();
+    const month = this.calendarService.currentMonthIndex();
+    const year = 2025;
+
+    const block = this.localStorageService.blocks().find(
+      block => block.date === day && block.month === month && block.year === year
+    );
+
+    return block ? [...block.plans] : [];
+  }
 }
--- src/app/side-panel/tomorrow/tomorrow.ts
+++ src/app/side-panel/tomorrow/tomorrow.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { LocalStorageService, Plan } from '../../services/local-storage-service';
+import { CalendarService } from '../../services/calendar-service';
 
 @Component({
   selector: 'app-tomorrow',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './tomorrow.css'
 })
 export class Tomorrow {
+  public tomorrowsPlan = computed(() => this.updateTomorrowsPlan());
 
+  constructor(
+    private localStorageService: LocalStorageService,
+    private calendarService: CalendarService
+  ) {}
+
+  updateTomorrowsPlan(): Plan[] {
+    const day = this.calendarService.getCurrentDateDay() + 1;
+    const month = this.calendarService.currentMonthIndex();
+    const year = 2025;
+
+    const block = this.localStorageService.blocks().find(
+      block => block.date === day && block.month === month && block.year === year
+    );
+
+    return block ? [...block.plans] : [];
+  }
 }
