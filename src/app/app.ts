4ef980900f817e0c03424c284f202f049e284e72
Lucas Setiady <luca.setia@gmail.com>
2025-09-14 19:45:52 +1000

    Functionality: Added an open welcome message to inform user of application

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,19 @@ import { TopBar } from './top-bar/top-bar';
 import { SidePanel } from './side-panel/side-panel';
 import { CalendarView } from './calendar-view/calendar-view';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WelcomeMessage } from './welcome-message/welcome-message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBar, SidePanel, CalendarView, FontAwesomeModule],
+  imports: [RouterOutlet, TopBar, SidePanel, CalendarView, FontAwesomeModule, WelcomeMessage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('lifePlanner');
+  public isWelcomeMessage: boolean = true;
 
+  setIsWelcomeMessage(value: boolean): void {
+    this.isWelcomeMessage = value;
+  }
 }
